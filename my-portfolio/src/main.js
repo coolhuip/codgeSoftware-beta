f20d4cfeb1d02e15f6ee94bfe469e498c32a3712
Danny <cool.huip@gmail.com>
2023-05-27 07:23:34 -0400

    Complete preparation: default definitions

--- my-portfolio/src/main.js
+++ my-portfolio/src/main.js
@@ -1,4 +1,8 @@
-// We want the least cascading priority for modern-normalize.css
+// We want the LEAST cascading priority for modern-normalize.css
     // We MUST mind the import order.
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
+import '../styles/utils.css';
+// We want the HIGHEST cascading priority for utile.css
+    // Otherwise, it won't work.
+
