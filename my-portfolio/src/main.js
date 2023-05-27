e2905d8e2018c2fef265571f2a4f6faf6985b2c9
Danny <cool.huip@gmail.com>
2023-05-27 04:01:39 -0400

    Update references

--- my-portfolio/src/main.js
+++ my-portfolio/src/main.js
@@ -1,1 +1,4 @@
-import '../styles/style.css'
+// We want the least cascading priority for modern-normalize.css
+    // We MUST mind the import order.
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
