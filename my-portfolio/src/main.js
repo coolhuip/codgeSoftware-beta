621b69a51f13b31ea7a1f6b908d537f9df2220f9
Danny <cool.huip@gmail.com>
2023-05-28 02:06:32 -0400

    Import header.css

--- my-portfolio/src/main.js
+++ my-portfolio/src/main.js
@@ -2,6 +2,7 @@
     // We MUST mind the import order.
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
+import '../styles/components/header.css';
 import '../styles/utils.css';
 // We want the HIGHEST cascading priority for utile.css
     // Otherwise, it won't work.
