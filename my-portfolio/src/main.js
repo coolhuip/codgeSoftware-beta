0b90edc98de362dca943c47d3130bf9320ac686a
Danny <cool.huip@gmail.com>
2023-06-03 20:03:46 -0400

    Create featured.css

--- my-portfolio/src/main.js
+++ my-portfolio/src/main.js
@@ -5,6 +5,7 @@ import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
+import '../styles/components/featured.css';
 import '../styles/utils.css';
 // We want the HIGHEST cascading priority for utile.css
     // Otherwise, it won't work.
