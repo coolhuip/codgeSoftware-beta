c97a3cbd3b44d81ca7db625b6fe2ccfdac35c7cc
Danny <cool.huip@gmail.com>
2023-06-04 16:58:55 -0400

    Create mobile-nav.css

--- my-portfolio/src/main.js
+++ my-portfolio/src/main.js
@@ -9,6 +9,7 @@ import '../styles/components/featured.css';
 import '../styles/components/work.css';
 import '../styles/components/contact.css';
 import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
 import '../styles/utils.css';
 // We want the HIGHEST cascading priority for utile.css
     // Otherwise, it won't work.
