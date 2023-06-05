5d487387e6920a8e4735a06005dfcf055e0e4958
Danny <cool.huip@gmail.com>
2023-06-04 23:19:39 -0400

    Create dark-mode.js

--- my-portfolio/src/utils/dark-mode.js
+++ my-portfolio/src/utils/dark-mode.js
@@ -1,1 +1,7 @@
-const themeToggleBtns = document.querySelector('theme-toggle');
+const themeToggleBtns = document.querySelector('#theme-toggle');
+
+themeToggleBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.body.classList.toggle('light-mode');
+    });
+});
